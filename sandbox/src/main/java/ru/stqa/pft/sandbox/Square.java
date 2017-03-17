faa2e581da14b738cdd8fd785159f79853b1a6bd
vi-babaeva <viktoriababaeva07@yandex.ru>
2017-03-17 16:34:33 +0300

    изменения по 1 уроку, модуль 15

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -4,4 +4,15 @@ package ru.stqa.pft.sandbox;
  * Created by dequa on 16.03.17.
  */
 public class Square {
+
+  public double l;
+
+  public Square(double l){ /* конструктор */
+    this.l=l;
+  }
+
+  public double area () {
+    return this.l  * this.l;
+  }
+
 }
