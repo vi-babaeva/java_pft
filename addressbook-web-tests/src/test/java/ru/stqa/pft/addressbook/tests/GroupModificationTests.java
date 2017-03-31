a91a1edc51a24cb6f0cf05354f108554b7fe1f72
vi-babaeva <viktoriababaeva07@yandex.ru>
2017-03-31 23:01:43 +0300

    создан GroupModificationTests (2 урок 12 модуль)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupsHelper().selectGroup();
+    app.getGroupsHelper().initGroupModification();
+    app.getGroupsHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupsHelper().submitGroupModification();
+    app.getGroupsHelper().returnToGroupPage();
+  }
+}
