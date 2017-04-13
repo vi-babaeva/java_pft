f6d6ed2e2ee599eb0075427f7de6e2f54243890e
vi-babaeva <viktoriababaeva07@yandex.ru>
2017-04-13 18:45:01 +0300

    вычисление максимально идентификатора в ContactCreationTests - урок 4 модуль 9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -12,18 +13,12 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Ivan", "Ivanov", "St.Petersburg", "+78880007733", "test@yandex.ru", "test1");
+    ContactData contact = new ContactData("Petr", "Ivanov", "St.Petersburg", "+78880007733", "test@yandex.ru", "test1");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -17,7 +17,7 @@ public class ContactModificationTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().selectEditButton();
-    ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"Ivan", "Ivanov", "St.Petersburg", "+78880007733", "test@yandex.ru", null);
+    ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"Ivan", "Smirnov", "St.Petersburg", "+78880007733", "test@yandex.ru", null);
     app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().selectUpdateButton();
     app.getNavigationHelper().goToHomePage();
