52f40e250cc11beabc11d55da618d59a86c7c6a4
vi-babaeva <viktoriababaeva07@yandex.ru>
2017-05-03 23:32:19 +0300

    выполнены урок 6 модуль 11 (Протоколирование (лог-файлы)) и урок 7 модуль 2 (Object Relational Mapping (ORM))

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -54,7 +56,7 @@ public class GroupCreationTests extends TestBase {
 
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
-    String[] names = new String[] {"test1", "test2", "test3"};
+    //String[] names = new String[] {"test1", "test2", "test3"};
       app.goTo().groupPage();
       Groups before = app.group().all();
       app.group().create(group);
